0ec3cb9503a5da33f153fb55811ad0ab4c2aac97
Musa jahun mac <msjahun@live.com>
2024-12-18 12:12:19 +0100

    worked on refactoring and more

--- src/sample_function/sample.ts
+++ src/sample_function/sample.ts
@@ -1,4 +1,4 @@
-import {onDocumentCreated} from "firebase-functions/v2/firestore";
+import {onDocumentCreated, onDocumentDeleted} from "firebase-functions/v2/firestore";
 
 export const onUserCreated = onDocumentCreated("users/{userId}", (event) => {
     // Get an object representing the document
@@ -13,5 +13,24 @@ export const onUserCreated = onDocumentCreated("users/{userId}", (event) => {
     // access a particular field as you would any JS property
     const name = data.name;
 
+    // perform more operations ...
+});
+
+
+export const onUserDeleted = onDocumentDeleted("users/{userId}", (event) => {
+    // Get an object representing the document
+    // e.g. {'name': 'Marie', 'age': 66}
+    const userId  = event.params.userId;
+    const snapshot = event.data;
+    if (!snapshot) {
+        console.log("No data associated with the event");
+        return;
+    }
+    //const data = snapshot.data() as { name: string, age: string };
+
+    // access a particular field as you would any JS property
+   // const name = data.name;
+
+    console.log("We have deleted user with id ",userId )
     // perform more operations ...
 });
