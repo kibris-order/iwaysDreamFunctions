39f08d7d5880a6fa573507039b2a3d0e6ce681a7
musa jahun <msjahun@live.com>
2024-12-23 04:26:48 +0100

    made a lot of progress, even deployed a cloud function that works

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ import * as admin from "firebase-admin";
 admin.initializeApp();
 admin.firestore().settings({ignoreUndefinedProperties: true});
 exports.sample_function = require("./sample_function/sample");
+exports.auth_function = require("./authentication/users");
