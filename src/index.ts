482c5d822adcadfd0af465c1164be7112220641e
musa jahun <msjahun@live.com>
2025-12-18 10:54:46 +0100

    added automatic deployment for github action for our functions project

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,4 @@ import * as admin from "firebase-admin";
 
 admin.initializeApp();
 admin.firestore().settings({ignoreUndefinedProperties: true});
-exports.sample_function = require("./sample_function/sample");
 exports.auth_function = require("./authentication/users");
-
-exports.counters= require('./count/counter');
-exports.customer_update_cascade = require("./onupdated/customersUpdate");
-exports.counters = require('./count/counter');
-exports.dashboard_graph_expenses = require('./dashboard_and_graphs/graph-expenses');
-exports.dashboard_graph_sales_expenses = require('./dashboard_and_graphs/graph-sales-expenses');
-exports.dashboard_graph_sales_expenses_customer = require('./dashboard_and_graphs/graph-sales-expenses-customer');
-exports.customer_calculate_totals = require('./dashboard_and_graphs/receivable-and-expenses-customer');
-exports.company_calculate_totals = require('./dashboard_and_graphs/company-totals');
